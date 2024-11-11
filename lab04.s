1c65409aa9c82763e72bb393070a695dac2c4290
andxros <149706968+andxros@users.noreply.github.com>
2024-11-11 10:47:34 +0200

    Completed Lab04

--- lab04.s
+++ lab04.s
@@ -14,7 +14,7 @@ marianna: .string "Marianna"
 # The labels below are replaced by the respective addresses
 arraySorted:    .word maria, marianna, marios, markos
 
-arrayNotSorted: .word marianna, markos, maria
+arrayNotSorted: .word marianna, markos, maria, marios 
 
 .text
 
@@ -26,24 +26,65 @@ arrayNotSorted: .word marianna, markos, maria
             ecall
 
 str_ge:
-#---------
-# Write the subroutine code here
-#  You may move jr ra   if you wish.
-#---------
-            jr   ra
+
+loop:                   
+            lbu    t0, 0(a0)
+            lbu    t1, 0(a1)
+            
+            # check if: a word is completed / words are equal / character == '\0'
+            bne    t0,zero, if_t0_<_t1 #if t0 !=0: return
+            bne    t1,zero, if_t0_<_t1 #if t1 !=0: return
+            
+            j      return_if_t0==zero_&&_t1==zer0
+if_t0_<_t1:
+            blt    t0, t1, return_if_t0_<_t1
+if_t0_>_t1:           
+            blt    t1, t0, return_if_t0_>_t1
+            
+            #take next character from string
+            addi    a0, a0, 1
+            addi    a1, a1, 1
+            j    loop
+return_if_t0_<_t1:
+            addi    a0, zero, 0
+            jr    ra
+return_if_t0_>_t1:
+            addi    a0, zero, 1
+            jr    ra
+return_if_t0==zero_&&_t1==zer0:
+            addi    a0, zero, 1
+            jr    ra
  
 # ----------------------------------------------------------------------------
-# recCheck(array, size)
-# if size == 0 or size == 1
-#     return 1
-# if str_ge(array[1], array[0])      # if first two items in ascending order,
-#     return recCheck(&(array[1]), size-1)  # check from 2nd element onwards
-# else
-#     return 0
 
 recCheck:
-#---------
-# Write the subroutine code here
-#  You may move jr ra   if you wish.
-#---------
+            slti t0, a1,   2 # t0 = a1*4
+            beq  t0, zero, check_Array_i_and_Array_i_plus_1
+            addi a0, zero, 1  # if size =0,1:
+            jr   ra           #     return a0 = 1
+check_Array_i_and_Array_i_plus_1:
+    
+            #push to stack
+            addi sp, sp,   -12
+            sw   ra, 8(sp)
+            sw   a0, 4(sp)
+            sw   a1, 0(sp)
+            
+            #check 
+            lw   a1, 0(a0)  # a1 = array[i]
+            lw   a0, 4(a0)  # a0 = array[i+1] 
+            jal  str_ge 
+            beq  a0, zero, return_recCheck  # if a0 == 0: array is not sorted -> return 
+            
+            #pull from stack
+            lw   a0, 4(sp)    # get a0 from stack
+            lw   a1, 0(sp)    # get a1 from stack
+            addi a0, a0,   4   # i = i + 1
+            
+            #recursion
+            addi a1, a1,   -1  # size-1
+            jal  recCheck
+return_recCheck:
+            lw   ra, 8(sp)
+            addi sp, sp,   12
             jr   ra
